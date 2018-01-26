ac8c792addd7feca8d582cbf11abce52bfc9b8ed
lilsista <darasse.deborah@gmail.com>
2018-01-26 17:38:32 +0100

    slier version 1

--- js/carousel.js
+++ js/carousel.js
@@ -6,20 +6,38 @@ $(document).ready(function(){
 	})*/
 
 	var pos = 0;
-	$('.prev').click(function(e){
-		pos = pos + 20;
+	$(".prev").css("display","none");
+	var nbImage = $('#slider-list img').length;
+	var divWidth = $('.slider').width();
+	var figure = $('#slider-list figure').width() + (parseInt($('#slider-list figure').css("margin-left")))*2 + (parseInt($('#slider-list figure').css("border-width")))*2;
+	
+	$('.next').click(function(e){
+		pos = pos + 100;
+		nbImage = nbImage - Math.round(divWidth/figure);
 		e.preventDefault();
 		$("#slider-list").animate({left : "-"+pos+"%"}, 2000  ,function(){
-			console.log(pos +" "+ $("#slider-liste img").length);
+			$(".prev").css("display","block");
+			if(nbImage<=Math.round(divWidth/figure)){
+				$(".next").css("display","none");
+
+			}		
+			
+			console.log(pos+" "+divWidth +" "+ $("#slider-list").css('width') +" "+nbImage);
+			
 		});
 	})
 
-	$('.next').click(function(e){
-		pos = pos - 20;
+	$('.prev').click(function(e){
+		pos = pos - 100;
+		nbImage = nbImage + Math.round(divWidth/figure);
 		e.preventDefault();
 		$("#slider-list").animate({left : "-"+pos+"%"}, 2000  ,function(){
-			console.log(pos);
-		});
+			$(".next").css("display","block");
+			if(pos <= 0){
+				$(".prev").css("display","none");
+			}
+			
+			console.log(pos +" "+ $("#slider-list img").length +" "+ $(this).find('li:last-child p').text());	});
 	})
 
 });
