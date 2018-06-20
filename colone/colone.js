c9098bba83599493cbfe1fc4f6d6d75c00a796a3
lilsista <darasse.deborah@gmail.com>
2018-06-20 16:59:39 +0200

    ajout slider

--- colone/colone.js
+++ colone/colone.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
 
-    var miniature = $('.miniature');
+    /*var miniature = $('.miniature');
     var normal = $('.img-normal');
     var fermer = $('.croix');
 
     miniature.click(function(){
-       /* normal.css({display : "block"});*/
+        normal.css({display : "block"});
         normal.effect("slide","slow");
     });
 
     fermer.click(function(){
-       /* normal.css({display : "none"});*/
+        normal.css({display : "none"});
        normal.effect("drop","slow");
+    });*/
+
+    $('.dropdown-toggle').click(function(){
+        $('.sous-menu').toggle("slow");
     });
 
 
